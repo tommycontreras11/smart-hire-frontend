2d34fb23a18586482995ba761bc78c648b9716db
TommyGC11 <tommycontreras34@gmail.com>
2025-05-22 16:38:19 -0400

    feat: add sample data structure for sidebar navigation

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -16,6 +16,8 @@ import { MainNav } from "./nav-main";
 
 import { NavUser } from "./nav-user";
 import { TeamSwitcher } from "./team-switcher";
+import { UserRoleEnum } from "@/enums/common.enum";
+import { useAuth } from "@/contexts/auth-context";
 
 // This is sample data.
 const data: IAppSidebarProps = {
@@ -36,14 +38,22 @@ const data: IAppSidebarProps = {
       icon: Home,
       isActive: true,
       visibleProps: {
-        bothRoles: true,
-        default: true,
+        userRole: UserRoleEnum.EMPLOYEE,
+      },
+    },
+    {
+      name: "Vacancies",
+      url: "/recruiter/vacancies",
+      icon: Home,
+      isActive: true,
+      visibleProps: {
+        userRole: UserRoleEnum.RECRUITER,
       },
     }
   ],
 };
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  // const { user } = useAuth();
+  const { user } = useAuth();
 
   return (
     <Sidebar collapsible="icon" {...props}>
@@ -52,13 +62,12 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <MainNav
-          // items={data.mainNav.filter((nav) =>
-          //   !user?.uuid
-          //     ? nav.visibleProps?.default
-          //     : nav.visibleProps?.userRole == user.role ||
-          //       nav.visibleProps?.bothRoles
-          // )}
-          items={data.mainNav}
+          items={data.mainNav.filter((nav) =>
+            !user?.uuid
+              ? nav.visibleProps?.default
+              : nav.visibleProps?.userRole == user.role ||
+                nav.visibleProps?.bothRoles
+          )}
         />
       </SidebarContent>
       <SidebarFooter>
